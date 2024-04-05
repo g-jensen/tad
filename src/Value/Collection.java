78d53f50fbd4f62dda9ffce109e59e459834a7c6
g-jensen <greg.micah.jensen@gmail.com>
2024-04-05 13:13:21 -0400

    add Set and set operations

--- src/Value/Collection.java
+++ src/Value/Collection.java
@@ -1,4 +1,14 @@
 
 public interface Collection extends Value {
   public boolean contains(Value v);
+
+  public static Collection union(Set c1, Set c2) {
+    return new UnionSet(c1, c2);
+  }
+  public static Collection intersection(Set c1, Set c2) {
+    return new IntersectionSet(c1, c2);
+  }
+  public static Collection difference(Set c1, Set c2) {
+    return new DifferenceSet(c1, c2);
+  }
 }
--- src/Value/FiniteSet.java
+++ src/Value/FiniteSet.java
@@ -1,10 +1,9 @@
 import java.util.HashSet;
-import java.util.Set;
 
-public class FiniteSet implements Collection {
-  private Set<Value> elements;
+public class FiniteSet implements Set {
+  private java.util.Set<Value> elements;
 
-  public FiniteSet(Set<Value> set) {
+  public FiniteSet(java.util.Set<Value> set) {
     elements = new HashSet<>(set);
   }
 
