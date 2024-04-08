7f93b9fc9120263a7fd4dc23fc354a4bba945572
g-jensen <greg.micah.jensen@gmail.com>
2024-04-08 18:12:38 -0400

    clean up FiniteSet

--- src/Value/FiniteSet.java
+++ src/Value/FiniteSet.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class FiniteSet implements Set {
   private java.util.Set<Value> elements;
@@ -11,11 +13,32 @@ public class FiniteSet implements Set {
     return elements.contains(v);
   }
 
+  public java.util.Set<Value> getElements() {
+    return elements;
+  }
+
   @Override
   public boolean equals(Object obj) {
     if (this == obj) {return true;}
     if (!(obj instanceof FiniteSet)) {return false;}
     FiniteSet fs = (FiniteSet)obj;
-    return this.elements.equals(fs.elements);
+    ArrayList<Value> a1 = new ArrayList<>(this.elements);
+    ArrayList<Value> a2 = new ArrayList<>(fs.elements);
+    return a1.containsAll(a2) && a2.containsAll(a1);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    Iterator<Value> it = elements.iterator();
+    sb.append("{");
+    while (it.hasNext()) {
+      sb.append(it.next().toString());
+      if (it.hasNext()) {
+        sb.append(", ");
+      }
+    }
+    sb.append("}");
+    return sb.toString();
   }
 }
